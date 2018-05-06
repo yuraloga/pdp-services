e67cdc90c1d7c75f9f3e69f768617bf5c95974bf
yura.logazyak <yura.logazyak@gmail.com>
2018-05-07 00:36:36 +0300

    Added config-service

--- config-service/src/main/java/com/yuraloga/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/yuraloga/configservice/ConfigServiceApplication.java
@@ -2,10 +2,13 @@ package com.yuraloga.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
-@SpringBootApplication
+@EnableEurekaClient
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
