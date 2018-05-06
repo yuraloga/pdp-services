dfe6073409f40785a6f26adb481277611d483871
yura.logazyak <yura.logazyak@gmail.com>
2018-05-07 00:37:35 +0300

    Added discovery-service

---
+++ discovery-service/src/main/java/com/yuraloga/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.yuraloga.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+}
