16e9b65b315a698d24f8320af76910fa2c6c471f
yura.logazyak <yura.logazyak@gmail.com>
2018-07-18 00:44:43 +0300

    Added simple swagger configuration

---
+++ ecar-service/src/main/java/com/yuraloga/ecarservice/configuration/SwaggerConfiguration.java
@@ -0,0 +1,39 @@
+package com.yuraloga.ecarservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.service.ApiInfo.DEFAULT_CONTACT;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yuraloga.ecarservice.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Ecar Service Documentation",
+                "Ecar API Documentation",
+                "1.0",
+                null,
+                DEFAULT_CONTACT,
+                null,
+                null
+        );
+    }
+}
