f295e3dc41eb5a0b9cce319189bf95f1bba126ab
yura.logazyak <yura.logazyak@gmail.com>
2018-07-19 00:31:16 +0300

    Added handling of NotFoundException

--- ecar-service/src/main/java/com/yuraloga/ecarservice/controller/ExceptionHandlerController.java
+++ ecar-service/src/main/java/com/yuraloga/ecarservice/controller/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package com.yuraloga.ecarservice.controller;
 
+import com.yuraloga.ecarservice.exceptions.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +15,11 @@ public class ExceptionHandlerController {
     public ResponseEntity<String> handleException(Throwable t) {
         log.error("Error: {}", t.getMessage());
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleException(NotFoundException e) {
+        log.warn("Entity was not found: {}", e.getMessage());
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
